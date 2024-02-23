47f7379d5e334a16f145896864960a588fac2019
Amanda-Lexine Sunga <amanda-lexine@Amandas-MacBook-Pro-2.local>
2024-02-22 20:09:28 -0500

    just added a little line

--- MODIFY_HERE.py
+++ MODIFY_HERE.py
@@ -3,5 +3,6 @@ def hello():
     print("Nicole and Aliyah are teaching Git!")
     
     # TO DO: Add your name / a message here!
+    print("look now there's another string here this is crazy")
 
 hello()
